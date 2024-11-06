6355d577f1c1b8c111c92dbfb846094a49cbc7b9
wittyvishnu <vishnuwitty@gmail.com>
2024-11-06 18:39:26 +0530

    commit the changes

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+document.getElementsByClassName("link1")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.remove("test");
+    document.getElementsByClassName("popular")[0].classList.remove("test");
+    document.getElementsByClassName("package")[0].classList.remove("test");
+    document.getElementsByClassName("gallery")[0].classList.remove("test");
+    document.getElementsByClassName("cta")[0].classList.remove("test");
+  });
+  document.getElementsByClassName("link2")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.remove("test");
+    document.getElementsByClassName("popular")[0].classList.add("test");
+    document.getElementsByClassName("package")[0].classList.add("test");
+    document.getElementsByClassName("gallery")[0].classList.add("test");
+    document.getElementsByClassName("cta")[0].classList.add("test");
+  });
+  document.getElementsByClassName("link3")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.add("test");
+    document.getElementsByClassName("popular")[0].classList.remove("test");
+    document.getElementsByClassName("package")[0].classList.add("test");
+    document.getElementsByClassName("gallery")[0].classList.add("test");
+    document.getElementsByClassName("cta")[0].classList.add("test");
+  });
+  document.getElementsByClassName("link4")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.add("test");
+    document.getElementsByClassName("popular")[0].classList.add("test");
+    document.getElementsByClassName("package")[0].classList.remove("test");
+    document.getElementsByClassName("gallery")[0].classList.add("test");
+    document.getElementsByClassName("cta")[0].classList.add("test");
+  });
+  document.getElementsByClassName("link5")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.add("test");
+    document.getElementsByClassName("popular")[0].classList.add("test");
+    document.getElementsByClassName("package")[0].classList.add("test");
+    document.getElementsByClassName("gallery")[0].classList.remove("test");
+    document.getElementsByClassName("cta")[0].classList.add("test");
+  });
+  document.getElementsByClassName("link6")[0].addEventListener("click", function() {
+
+    document.getElementsByClassName("hero")[0].classList.remove("test");
+    document.getElementsByClassName("tour-search")[0].classList.add("test");
+    document.getElementsByClassName("popular")[0].classList.add("test");
+    document.getElementsByClassName("package")[0].classList.add("test");
+    document.getElementsByClassName("gallery")[0].classList.add("test");
+    document.getElementsByClassName("cta")[0].classList.remove("test");
+  });
